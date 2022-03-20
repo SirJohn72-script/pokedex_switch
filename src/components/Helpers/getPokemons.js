5615ad6be365e1f7cfd36722895735ccf944b347
Juan Glez <85137676+SirJohn72-script@users.noreply.github.com>
2022-03-20 02:42:57 -0600

    Update getPokemons.js

--- src/components/Helpers/getPokemons.js
+++ src/components/Helpers/getPokemons.js
@@ -7,7 +7,6 @@ export const getListPokemons = async (offset = 0) => {
   try {
     const res = await axios.get(url)
     const pokemons = await res.data.results
-    // console.log(pokemons)
     let pokemosList = []
     for (let poke of pokemons) {
       await getPokemon(poke.url).then((data) => {
